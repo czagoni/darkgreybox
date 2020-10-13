d8162179006f6ec1b59014e1c37c51652c580daf
csaba <csaba@upsideenergy.co.uk>
2020-10-13 12:39:56 +0200

    docs: added docstrings to fit functions

--- darkgreybox/fit.py
+++ darkgreybox/fit.py
@@ -7,6 +7,53 @@ from joblib import Parallel, delayed
 
 def train_models(models, X_train, y_train, error_metric,
                  splits=None, method='nelder', n_jobs=-1, verbose=10):
+    """
+    Trains the `models` for the given `X_train` and `y_train` training data
+    for `splits` using `method`. 
+
+    Params:
+        models: list of `model.DarkGreyModel` objects
+            list of models to be trained
+        X_train: `pandas.DataFrame`
+            A pandas DataFrame of the training input data X
+        y_train: `pandas.Series`
+            A pandas Series of the training input data y
+        error_metric: function
+            An error metric function that confirms to the `sklearn.metrics` interface
+        splits: list
+            A list of training data indices specifying sub-sections of `X_train` and `y_train` 
+            for the models to be trained on
+        method : str
+            Name of the fitting method to use. Valid values are described in:
+            `lmfit.minimize`
+        n_jobs: int
+            The number of parallel jobs to be run as described by `joblib.Parallel`
+        verbose: int
+            The degree of verbosity as described by `joblib.Parallel`
+
+    Returns:
+        `pandas.DataFrame` with a record for each model's result for each split
+
+    Example:
+    ~~~~
+    
+    from sklearn.metrics import mean_squared_error
+    from sklearn.model_selection import KFold
+
+    from darkgreybox.model import TiTe
+    from darkgreybox.fit import train_models
+
+
+    prefit_df = train_models(models=[TiTe(train_params, rec_duration=1)],
+                             X_train=X_train, 
+                             y_train=y_train, 
+                             splits=KFold(n_splits=int(len(X_train) / 24), shuffle=False).split(X_train), 
+                             error_metric=mean_squared_error,
+                             method='nelder', 
+                             n_jobs=-1, 
+                             verbose=10)
+    ~~~~
+    """
 
     if n_jobs != 1:
         with Parallel(n_jobs=n_jobs, verbose=verbose) as p:
@@ -21,6 +68,26 @@ def train_models(models, X_train, y_train, error_metric,
 
 
 def train_model(base_model, X_train, y_train, error_metric, method='nelder'):
+    """
+    Trains a copy of `basemodel` for the given `X_train` and `y_train` training data
+    using `method`. 
+
+    Params:
+        base_model: `model.DarkGreyModel`
+            model to be trained (a copy will be made)
+        X_train: `pandas.DataFrame`
+            A pandas DataFrame of the training input data X
+        y_train: `pandas.Series`
+            A pandas Series of the training input data y
+        error_metric: function
+            An error metric function that confirms to the `sklearn.metrics` interface
+        method : str
+            Name of the fitting method to use. Valid values are described in:
+            `lmfit.minimize`
+
+    Returns:
+        `pandas.DataFrame` with a single record for the fit model's result 
+    """
 
     start = timer()
     model = copy.deepcopy(base_model)
@@ -53,8 +120,22 @@ def train_model(base_model, X_train, y_train, error_metric, method='nelder'):
 
 
 def get_ic_params(model, X_train):
+    """
+    Returns the initial condition parameters of a model from the training data
 
-    # TODO: make this more robust
+    Params:
+        model: `model.DarkGreyModel`
+            model to get initial condition parameters from
+        X_train: `pandas.DataFrame`
+            A pandas DataFrame of the training input data X    
+
+    Returns:
+        A dictionary containing the initial conditions and their corresponding values 
+        as defined by the training data
+
+    """
+
+    # TODO: this is horrible - make this clearer and more robust
     ic_params = {}
     for key in model.params.keys():
         if '0' in key:
